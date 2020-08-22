376bbd603bda7043195813b8d359b10a7aa0d01b
Bharath Kumar <pbkbharath1707@gmail.com>
2020-08-22 13:26:34 +0530

    Updates

---
+++ precache-manifest.5f8174b782ca0aa36ce92127ca3600e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3177acd99d3c44e241eb1b79eb8779",
+    "url": "/Google-Bharath/index.html"
+  },
+  {
+    "revision": "e2b13d56585061897cd2",
+    "url": "/Google-Bharath/static/css/main.fd55ce28.chunk.css"
+  },
+  {
+    "revision": "f38d7a6a3173dd1ca45c",
+    "url": "/Google-Bharath/static/js/2.5d3d0092.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Google-Bharath/static/js/2.5d3d0092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b13d56585061897cd2",
+    "url": "/Google-Bharath/static/js/main.ec25c8a6.chunk.js"
+  },
+  {
+    "revision": "590a75e7ff3feea9f338",
+    "url": "/Google-Bharath/static/js/runtime-main.db808720.js"
+  }
+]);
